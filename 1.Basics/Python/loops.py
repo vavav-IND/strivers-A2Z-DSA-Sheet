6391da1b37766cdd59946deb2eca949880db51f9
vavav-IND <momoeater67420@gmail.com>
2026-06-19 09:24:20 +0530

    Delete 1.Basics/Python directory

--- 1.Basics/Python/loops.py
+++
@@ -1,6 +0,0 @@
-low=int(input('low='))
-high=int(input('high='))
-total=0
-for i in range(low, high+1):
-    total=total+i
-print(total)
--- 1.Basics/Python/while.py
+++
@@ -1,7 +0,0 @@
-d=int(input('d='))
-total=0
-i=d
-while(i<491+d):
-    total=total+i
-    i+=10
-print(total)

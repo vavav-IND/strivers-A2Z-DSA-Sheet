9c4bcd552061c4b408f1f28bd392d4ecadc258f3
vavav-IND <momoeater67420@gmail.com>
2026-06-26 19:06:36 +0530

    all easy questions of array

---
+++ 3. ARRAYS/1. EASY/10. max1s.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count=0;
+        int maxcount=0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]==0){
+                count=0;
+            }
+            else{
+                count++;
+                if(maxcount<count){
+                    maxcount=count;
+                }
+            }
+        }
+        return maxcount;
+    }
+};
---
+++ 3. ARRAYS/1. EASY/11. freqis1.cpp
@@ -0,0 +1,14 @@
+class Solution{    
+public:    
+    int singleNumber(vector<int>& nums){
+        map<int,int>m;
+        for(int i=0; i<nums.size(); i++){
+            m[nums[i]]++;
+        }
+        for(auto it:m){
+            if(it.second==1){
+                return it.first;
+            }
+        }
+    }
+};

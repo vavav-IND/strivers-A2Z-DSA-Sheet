fce5a27b45a6f4aa44e398f097b36642470c7782
vavav-IND <momoeater67420@gmail.com>
2026-08-17 22:29:42 +0530

    medium questions of array done

--- 03. ARRAYS/2. MEDIUM/02. sortcolours.cpp
+++ 03. ARRAYS/2. MEDIUM/02. sortcolours.cpp
@@ -39,3 +39,29 @@ public:
         }
     }
 };
+
+//Dutch national flag algo
+class Solution {
+public:
+    void sortZeroOneTwo(vector<int>& nums) {
+        int low=0;
+        int mid=0;
+        int high=nums.size()-1;
+        while(mid<=high){
+                if(nums[mid]==0){
+                    swap(nums[mid], nums[low]);
+                    low++;
+                    mid++;
+                }
+                else if(nums[mid]==1){
+                
+                    mid++;
+                }
+
+                else{
+                    swap(nums[mid], nums[high]);
+                    high--;
+                }
+        }
+    }
+};
